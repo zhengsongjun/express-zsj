016393a90783d636acdac4cd639d1fc442faf68f
zhengsongjun <zheng_songjun@venusgroup.com.cn>
2023-03-21 21:33:53 +0800

    feat: express-validator初体验

--- controller/userController.js
+++ controller/userController.js
@@ -1,11 +1,13 @@
 const { User } = require('../model/index')
 
 exports.register = async (req,res) => {
-	const userModel = new User(req.body)
-	const dbBack = await userModel.save()
-	const userDbBack = dbBack.toJSON()
-	delete userDbBack.password
-	res.status(201).json({userDbBack})
+	console.log(req.body)
+	return 
+	// const userModel = new User(req.body)
+	// const dbBack = await userModel.save()
+	// const userDbBack = dbBack.toJSON()
+	// delete userDbBack.password
+	// res.status(201).json({userDbBack})
 }
 
 exports.list = async (req,res) => {
--- router/user.js
+++ router/user.js
@@ -1,8 +1,18 @@
 const express = require('express')
 const router = express.Router()
 const useController = require('../controller/userController')
+
+const { body , validationResult } = require('express-validator')
+
 router
-.post('/register',useController.register)
+.post('/register',body('username')
+.notEmpty().withMessage('用户名不能为空！')
+.isLength({min:3}).withMessage("用户名长度小于3!"),
+(req,res,next)=>{
+	const errors = validationResult(req);
+	console.log(errors);
+}
+,useController.register)
 .get('/list',useController.list)
 .delete('/',useController.delete)
 
