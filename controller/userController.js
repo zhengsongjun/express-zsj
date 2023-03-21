e47c5e61a743a93cb152201e9c673cd80120eaee
zhengsongjun <zheng_songjun@venusgroup.com.cn>
2023-03-21 22:11:26 +0800

    feat: 用户注册数据唯一性验证完成

--- controller/userController.js
+++ controller/userController.js
@@ -1,13 +1,11 @@
 const { User } = require('../model/index')
 
 exports.register = async (req,res) => {
-	console.log(req.body)
-	return 
-	// const userModel = new User(req.body)
-	// const dbBack = await userModel.save()
-	// const userDbBack = dbBack.toJSON()
-	// delete userDbBack.password
-	// res.status(201).json({userDbBack})
+	const userModel = new User(req.body)
+	const dbBack = await userModel.save()
+	const userDbBack = dbBack.toJSON()
+	delete userDbBack.password
+	res.status(201).json({userDbBack})
 }
 
 exports.list = async (req,res) => {
--- middleware/validator/userValidator.js
+++ middleware/validator/userValidator.js
@@ -1,10 +1,27 @@
 const { body  } = require('express-validator');
 const validate = require('../validator/errorBack')
+const { User } = require('../../model/index')
+
 
 module.exports.register = validate([
 	body('username')
 		.notEmpty().withMessage('用户名不能为空！').bail()
 		.isLength({min:3}).withMessage("用户名长度小于3!"),
 	body('email')
-		.notEmpty().withMessage('邮箱不能为空')
+		.notEmpty().withMessage('邮箱不能为空').bail()
+		.isEmail().withMessage('邮箱格式不正确').bail()
+		.custom(async val => {
+			const emailValidate = await User.findOne({email:val})
+			if(emailValidate){
+				return Promise.reject("邮箱已被注册")
+			}
+		}),
+	body('phone')
+		.notEmpty().withMessage('手机号不能为空').bail()
+		.custom(async val => {
+			const phoneValidate = await User.findOne({phone:val})
+			if(phoneValidate) {
+				return Promise.reject("手机号已注册")
+			}
+		}).bail()
 ])
